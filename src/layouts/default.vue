07c095c11cbd91c8ff2fefde6beba54c1e1049b8
wu <wooyanhong@163.com>
2018-04-24 23:24:31 +0800

    Switch the UI Theme to ios style

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,7 +2,7 @@
   <q-layout view="LHh Lpr LFf">
     <q-layout-header style="-webkit-app-region: drag">
       <q-toolbar color="primary"
-                 glossy
+                 :glossy="$q.theme === 'mat'"
                  :inverted="$q.theme === 'ios'">
         <q-toolbar-title>
           <q-icon :name="toolbarIcon" /> <span>{{ toolbarTitle }}</span>
