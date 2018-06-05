7b232d30828e999334d597b731b37e8702fd9a4b
wuynng <wuynng@163.com>
2018-06-05 18:21:25 +0800

    Config electron-builder for mac platform; updated linux icon; clean package.json.

--- quasar.conf.js
+++ quasar.conf.js
@@ -76,51 +76,7 @@ module.exports = function (ctx) {
       // Quasar plugins
       plugins: ['Notify', 'Loading', 'Dialog']
     },
-    // animations: 'all' --- includes all animations
     animations: 'all',
-    pwa: {
-      cacheExt:
-        'js,html,css,ttf,eot,otf,woff,woff2,json,svg,gif,jpg,jpeg,png,wav,ogg,webm,flac,aac,mp4,mp3',
-      manifest: {
-        // name: 'Quasar App',
-        // short_name: 'Quasar-PWA',
-        // description: 'Best PWA App in town!',
-        display: 'standalone',
-        orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
-        icons: [
-          {
-            src: 'statics/icons/icon-128x128.png',
-            sizes: '128x128',
-            type: 'image/png'
-          },
-          {
-            src: 'statics/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'statics/icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
-          },
-          {
-            src: 'statics/icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png'
-          },
-          {
-            src: 'statics/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    },
-    cordova: {
-      // id: 'org.cordova.quasar.app'
-    },
     electron: {
       extendWebpack (cfg) {
         // do something with cfg
@@ -128,18 +84,26 @@ module.exports = function (ctx) {
       beforePackaging: function (opts) {
         // do nothings
       },
-      packager: {
-        afterCopy: [(buildPath, electronVersion, platform, arch, callback) => {
-          const path = require('path')
-          const rebuild = require(path.resolve(__dirname, 'node_modules', 'electron-rebuild')).rebuild
+      // packager: {
+      //   afterCopy: [(buildPath, electronVersion, platform, arch, callback) => {
+      //     const path = require('path')
+      //     const rebuild = require(path.resolve(__dirname, 'node_modules', 'electron-rebuild')).rebuild
 
-          rebuild({ buildPath, electronVersion, arch })
-            .then(() => {
-              console.log('Run electron-rebuild OK')
-              callback()
-            })
-            .catch((error) => callback(error))
-        }]
+      //     rebuild({ buildPath, electronVersion, arch })
+      //       .then(() => {
+      //         console.log('Run electron-rebuild OK')
+      //         callback()
+      //       })
+      //       .catch((error) => callback(error))
+      //   }]
+      // },
+      // electron-builder options
+      builder: {
+        appId: 'io.smartmesh.ozone',
+        mac: {
+          category: 'public.app-category.education',
+          icon: 'src-electron/icons/icon.icns'
+        }
       }
     },
 
