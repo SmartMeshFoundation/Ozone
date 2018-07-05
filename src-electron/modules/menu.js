eb159daa5b7db8bb26a6680230a170c9b7797f67
wuynng <wuynng@163.com>
2018-07-05 23:19:32 +0800

    fixed app's menu issues on windows and linux

--- src-electron/modules/menu.js
+++ src-electron/modules/menu.js
@@ -286,6 +286,10 @@ class OzoneMenu {
         label: global.i18n.t('appMenu.file'),
         submenu: [
           {
+            role: 'about',
+            label: global.i18n.t('appMenu.about') + ' ' + app.getName()
+          },
+          {
             role: 'quit',
             label: global.i18n.t('appMenu.quit') + ' ' + app.getName()
           }
